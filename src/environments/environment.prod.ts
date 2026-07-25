44262c53d97ab247c70eaa26e2f0b9f273527fae
saurabh1420kumar-star <saurabh1420kumar@gmail.com>
2026-07-25 20:12:28 +0530

    Fix HR URL in environment configuration for production and development

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.imsnectarorigin.com/api',
   permissionsUrl: 'https://api.imsnectarorigin.com/api/permissions',
-  hrUrl: 'https://api.imsnectarorigin.com/api/hr',
+  hrUrl: 'https://api.imsnectarorigin.com/api',
   productsUrl: 'https://api.imsnectarorigin.com/api/products',
   distributorUrl: 'https://api.imsnectarorigin.com/api/distributors',
   ledgerUrl: 'https://api.imsnectarorigin.com/api/ledger',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   // Production server URLs
   apiUrl: 'https://api.imsnectarorigin.com/api',
   permissionsUrl: 'https://api.imsnectarorigin.com/api/permissions',
-  hrUrl: 'https://api.imsnectarorigin.com/api/hr',
+  hrUrl: 'https://api.imsnectarorigin.com/api',
   productsUrl: 'https://api.imsnectarorigin.com/api/products',
   distributorUrl: 'https://api.imsnectarorigin.com/api/distributors',
   ledgerUrl: 'https://api.imsnectarorigin.com/api/ledger',
